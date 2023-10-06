c74f1ff237766a47b0861ad8097ef74f83fe0587
jesus <jesus.antonio.garcia9871@gmail.com>
2023-10-06 00:26:33 -0500

    Component destination :)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import NavRoutes from './routes/NavRoutes.jsx';
-import {BrowserRouter} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import NavRoutes from "./routes/NavRoutes.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-      <NavRoutes/>
-    </BrowserRouter>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <NavRoutes />
+  </BrowserRouter>
+);
--- src/routes/NavRoutes.jsx
+++ src/routes/NavRoutes.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom';
-import NavBar from '../components/NavBar';
-import Home from '../pages/Home';
-import Destination from '../pages/Destination';
-import Crew from '../pages/Crew';
-import Technology from '../pages/Technology';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+//components
+import NavBar from "../components/NavBar";
+import Home from "../pages/Home";
+import Destination from "../pages/Destination/Destination";
+import Crew from "../pages/Crew";
+import Technology from "../pages/Technology";
 
 export const NavRoutes = () => {
   return (
     <>
       <Routes>
-          <Route element={<NavBar/>}>                    
-              <Route index element={<Home/>} />
-              <Route path='home' element={<Home/>} />
-              <Route path='destination' element={<Destination/>} />
-              <Route path='crew' element={<Crew/>} />
-              <Route path='technology' element={<Technology/>} />
-          </Route>
+        <Route element={<NavBar />}>
+          <Route index element={<Home />} />
+          <Route path="home" element={<Home />} />
+          <Route path="destination" element={<Destination />} />
+          <Route path="crew" element={<Crew />} />
+          <Route path="technology" element={<Technology />} />
+        </Route>
       </Routes>
     </>
-  )
-}
+  );
+};
 
 export default NavRoutes;
