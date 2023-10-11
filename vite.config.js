da5abe602b8b22dd941efaf178671a97af73b581
Wilmer Rodolfo Castro Ladino <wilmer.castro@unillanos.edu.co>
2023-10-10 22:22:20 -0500

    cambios

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ghPages  } from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),ghPages ({
+    branch: "main",
+  })],
   base: "/workshop2-s2/"
 })
